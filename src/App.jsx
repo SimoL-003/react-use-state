505b77693060942a35372ec2bc70359d237864ef
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-11-05 16:49:08 +0100

    feat: Create button list and use useState to update card content based on selected button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import { useState } from "react";
+import { languages } from "./data/languages"; /* ID, title, description */
 
 function App() {
-  return <></>;
+  const [activeCard, setActiveCard] = useState();
+  console.log(activeCard);
+
+  return (
+    <main className="min-h-screen">
+      <div className="container py-20">
+        <h1 className="mb-10">Impara lo sviluppo web</h1>
+
+        <div className="flex flex-wrap gap-5">
+          {languages.map((curLanguage) => (
+            <button
+              onClick={() => {
+                setActiveCard(curLanguage);
+              }}
+              key={curLanguage.id}
+              className="button"
+            >
+              {curLanguage.title}
+            </button>
+          ))}
+        </div>
+
+        <div className="card">
+          <h3>{activeCard.title}</h3>
+          <p>{activeCard.description}</p>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
